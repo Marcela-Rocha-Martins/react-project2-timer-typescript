8f62fcab5bc7942e775a5504bf94d1d6f5f669b1
marcela rocha <marcelarochamartins@gmail.com>
2024-01-08 14:36:22 +0100

    text corrections

--- src/Pages/Home/Components/NewCycleForm/index.tsx
+++ src/Pages/Home/Components/NewCycleForm/index.tsx
@@ -9,7 +9,7 @@ export function NewCycleForm() {
 
   return (
     <FormContainer>
-      <label htmlFor="task">Vou trabalhar em</label>
+      <label htmlFor="task">I will work at</label>
       <TaskInput
         id="task"
         list="task-suggestions"
